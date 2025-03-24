b4bd692d2ca15a457180e6cf3ed0aca121ebfeb4
Petrolbras <pietrogouvearepleop@gmail.com>
2025-03-24 18:06:33 -0300

    finalizado

--- index.js
+++ index.js
@@ -10,25 +10,32 @@ let x = 0
 let y = 0
 let velx = Math.round(3+Math.random()*5)
 let vely = Math.round(3+Math.random()*5)
+let assets = ['assets/alvorada.png', 'assets/bravo.png', 'assets/hyper.png', 'assets/marcelo.png', 'assets/marcus.png', 'assets/miyuki.png']
+let img = new Image()
+img.src = assets[Math.floor(Math.random()*6)]
+
 setInterval(() => {
 ctx.fillStyle = "white"
 ctx.fillRect(0, 0, 1200, 900)
-ctx.fillStyle = "orange"
-ctx.fillRect(x, y, 200, 100)
+ctx.drawImage(img, x, y, 150, 150)
 x = x + velx
 y = y + vely
 
-if (y >= 900-100) {
+if (y >= 900-150) {
    vely = vely*-1
+   img.src = assets[Math.floor(Math.random()*6)]
 }
-if (x >= 1200-200) {
+if (x >= 1200-150) {
    velx = velx*-1
+   img.src = assets[Math.floor(Math.random()*6)]
 }
 if (y <= 0) {
    vely = vely*-1
+   img.src = assets[Math.floor(Math.random()*6)]
 }
 if (x <= 0) {
    velx = velx*-1
+   img.src = assets[Math.floor(Math.random()*6)]
 }
 
 

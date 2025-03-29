9956957668c7c31ac0008e3f50a4a3fe77df2b67
Petrolbras <pietrogouvearepleop@gmail.com>
2025-03-29 03:29:47 -0300

    update

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
 const canvas = document.getElementById("mycanvas");
 const ctx = canvas.getContext("2d");
 
-let x = 0;
-let y = 0;
-let velx = Math.round(4 + Math.random() * 5);
-let vely = Math.round(4 + Math.random() * 5);
 let assets = [
-   "assets/alvorada.png",
-   "assets/bravo.png",
-   "assets/hyper.png",
-   "assets/marcelo.png",
-   "assets/marcus.png",
-   "assets/miyuki.png",
+	"assets/alvorada.png",
+	"assets/bravo.png",
+	"assets/hyper.png",
+	"assets/marcelo.png",
+	"assets/marcus.png",
+	"assets/miyuki.png"
 ];
-let img = new Image();
-let src = "";
-setImage();
+let bolinhas = []
+let tamanhox = 1200
+let tamanhoy = 900
+let tamanhoicone = 150
+criarbolinhas()
 
-function setImage() {
-   let probabilidade = Math.floor(Math.random() * 6);
-   while (src == assets[probabilidade]) {
-      probabilidade = Math.floor(Math.random() * 6);
-   }
-   img.src = assets[probabilidade];
-   src = assets[probabilidade];
+function criarbolinhas(){
+	let img = new Image();
+	setImage(img);
+
+	let velx = (6 + Math.random() * 7);
+	let vely = (4 + Math.random() * 7);
+	if (Math.round(Math.random()) == 1){
+		velx = velx *-1
+		vely = vely *-1
+	}
+	let x = (Math.random() * (tamanhox - tamanhoicone))
+	let y = (Math.random() * (tamanhoy - tamanhoicone))
+	bolinhas.push({x,y,velx,vely,img})
+}
+
+function setImage(img) {
+	let probabilidade = Math.floor(Math.random() * assets.length);
+	if (img.src.includes(assets[probabilidade])) {
+		probabilidade = (probabilidade + 1) % assets.length
+	}
+	img.src = assets[probabilidade];
 }
 
 setInterval(() => {
-   ctx.fillStyle = "white";
-   ctx.fillRect(0, 0, 1200, 900);
-   ctx.drawImage(img, x, y, 150, 150);
-   x = x + velx;
-   y = y + vely;
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, tamanhox, tamanhoy);
+	
 
-   if (y >= 900 - 150) {
-      vely = vely * -1;
-      setImage();
-   }
-   if (x >= 1200 - 150) {
-      velx = velx * -1;
-      setImage();
-   }
-   if (y <= 0) {
-      vely = vely * -1;
-      setImage();
-   }
-   if (x <= 0) {
-      velx = velx * -1;
-      setImage();
-   }
+	for(var i = 0; i < bolinhas.length; i++) {
+		ctx.drawImage(bolinhas[i].img, bolinhas[i].x, bolinhas[i].y, tamanhoicone, tamanhoicone);
+
+		bolinhas[i].x = bolinhas[i].x + bolinhas[i].velx;
+		bolinhas[i].y = bolinhas[i].y + bolinhas[i].vely;
+
+		if (bolinhas[i].y >= tamanhoy - tamanhoicone) {
+			bolinhas[i].vely = bolinhas[i].vely * -1;
+			setImage(bolinhas[i].img);
+		}
+		if (bolinhas[i].x >= tamanhox - tamanhoicone) {
+			bolinhas[i].velx = bolinhas[i].velx * -1;
+			setImage(bolinhas[i].img);
+		}
+		if (bolinhas[i].y <= 0) {
+			bolinhas[i].vely = bolinhas[i].vely * -1;
+			setImage(bolinhas[i].img);
+		}
+		if (bolinhas[i].x <= 0) {
+			bolinhas[i].velx = bolinhas[i].velx * -1;
+			setImage(bolinhas[i].img);
+		}
+	}
 }, 1000 / 60);

ef7cdc854cca4d0afa507afcdafba67832e8ff09
Petrolbras <pietrogouvearepleop@gmail.com>
2025-03-29 03:39:27 -0300

    att

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ let assets = [
 	"assets/marcus.png",
 	"assets/miyuki.png"
 ];
-let bolinhas = []
-let tamanhox = 1200
-let tamanhoy = 900
-let tamanhoicone = 150
-criarbolinhas()
+let bolinhas = [];
+let tamanhox = 1200;
+let tamanhoy = 900;
+let tamanhoicone = 150;
+criarbolinhas();
 
-function criarbolinhas(){
+function criarbolinhas() {
 	let img = new Image();
 	setImage(img);
 
-	let velx = (6 + Math.random() * 7);
-	let vely = (4 + Math.random() * 7);
-	if (Math.round(Math.random()) == 1){
-		velx = velx *-1
-		vely = vely *-1
+	let velx = 6 + Math.random() * 7;
+	let vely = 4 + Math.random() * 7;
+	if (Math.round(Math.random()) == 1) {
+		velx = velx * -1;
+		vely = vely * -1;
 	}
-	let x = (Math.random() * (tamanhox - tamanhoicone))
-	let y = (Math.random() * (tamanhoy - tamanhoicone))
-	bolinhas.push({x,y,velx,vely,img})
+	let x = Math.random() * (tamanhox - tamanhoicone);
+	let y = Math.random() * (tamanhoy - tamanhoicone);
+	bolinhas.push({ x, y, velx, vely, img });
 }
 
 function setImage(img) {
 	let probabilidade = Math.floor(Math.random() * assets.length);
 	if (img.src.includes(assets[probabilidade])) {
-		probabilidade = (probabilidade + 1) % assets.length
+		probabilidade = (probabilidade + 1) % assets.length;
 	}
 	img.src = assets[probabilidade];
 }
@@ -41,9 +41,8 @@ function setImage(img) {
 setInterval(() => {
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, tamanhox, tamanhoy);
-	
 
-	for(var i = 0; i < bolinhas.length; i++) {
+	for (var i = 0; i < bolinhas.length; i++) {
 		ctx.drawImage(bolinhas[i].img, bolinhas[i].x, bolinhas[i].y, tamanhoicone, tamanhoicone);
 
 		bolinhas[i].x = bolinhas[i].x + bolinhas[i].velx;
